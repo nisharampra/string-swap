505524a46524ede1e5131a1e993a9dd486aa29ad
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 23:17:03 +0800

    Add files via upload

---
+++ string-swap/string-swap/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  string-swap
+//
+//  Created by Nisha Ramprasath on 3/11/23.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    string str1="programming";
+    string str2="language";
+    str1.swap(str2);
+    cout<<str1<<endl;
+    cout<<str2<<endl;
+
+    return 0;
+}
